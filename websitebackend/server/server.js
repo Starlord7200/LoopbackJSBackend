c118821d07f84f8ada17dbb02b132dd25993f5c3
JulianReid541 <reidjulian@outlook.com>
2020-12-24 11:33:27 -0800

    added test observers to the backend

--- websitebackend/server/server.js
+++ websitebackend/server/server.js
@@ -32,3 +32,24 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+console.log(Object.keys(app.models));
+
+app.models.User.afterRemote('create', (ctx, user, next) => {
+  console.log("New user is", user);
+
+  app.models.Profile.create({
+    first_name: user.username,
+    created_at: new Date(),
+    userId: user.id
+  }, (err, result) => {
+    if(!err && result) {
+      console.log("Created the profile!", result);
+    }
+    else {
+      console.log("there is an error", err);
+    }
+    next();
+  });
+
+});
