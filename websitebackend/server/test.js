d8ba3fcefa155e43b3acd2756b725a58187e82e0
JulianReid541 <reidjulian@outlook.com>
2020-12-29 11:51:03 -0800

    added editor role and assigned authorization to api

--- websitebackend/server/test.js
+++ websitebackend/server/test.js
@@ -1,33 +1,32 @@
 var models = require('./server.js').models;
 
-// var toSave = [
-//     {name: 'test1', email: 'mail1@mail.com'},
-//     {name: 'test2', email: 'mail2@mail.com'},
-//     {name: 'test3', email: 'mail3@mail.com'},
-//     {name: 'test4', email: 'mail4@mail.com'},
-//     {name: 'test5', email: 'mail5@mail.com'},
-//     {name: 'test6', email: 'mail6@mail.com'},
-// ];
+var toSave = [
+    {name: 'test1', email: 'mail1@mail.com'},
+    {name: 'test2', email: 'mail2@mail.com'},
+    {name: 'test3', email: 'mail3@mail.com'},
+    {name: 'test4', email: 'mail4@mail.com'},
+    {name: 'test5', email: 'mail5@mail.com'},
+    {name: 'test6', email: 'mail6@mail.com'},
+];
 
-// toSave.map(obj => {
-//     models.Profile.create(obj, (err, created) => {
-//         console.log("Created?", created);
-//     })
-// })
-
-var filter = {
-    where: {
-        email: {like: 'mail'}
-    },
-    order: 'id ASC',
-    limit: 10,
-    skip: 0,
-    fields: {
-        email: true
-    }
-}
-
-models.Profile.findById("5fea0d4426f6af09a49f26df", {include: 'Posts'}, (err, found) => {
-    console.log("Found?", err, found);
-    found.Posts.destroyAll({date: {lte: new Date('2019-02-04')}});
+toSave.map(obj => {
+    models.Profile.create(obj, (err, created) => {
+        console.log("Created?", created);
+    })
 })
+
+// var filter = {
+//     where: {
+//         email: {like: 'mail'}
+//     },
+//     order: 'id ASC',
+//     limit: 10,
+//     skip: 0,
+//     fields: {
+//         email: true
+//     }
+// }
+
+// models.Profile.find("5fea0d4426f6af09a49f26df", {include: 'Posts'}, (err, found) => {
+//     console.log("Found?", err, found);
+// })
